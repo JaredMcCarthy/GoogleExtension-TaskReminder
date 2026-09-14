1f923ffd9cd2ea02d0c61428bf352ceb6e6c96cb
jared <jared.lopezmccarthy05@gmail.com>
2026-09-14 09:58:25 -0600

    avance en feauture nueva de JS

--- script.js
+++ script.js
@@ -1,30 +1,92 @@
 
 // Primeras instrucciones
-//estos se llaman para saber que caztegoria son cada tarea
-
-
+//id que llamamos desde html para usarlos aqui
 let addBtn = document.getElementById("btn-agregar")
 let addText = document.getElementById("input-text")
 let addDate = document.getElementById("input-date")
 
 
-let datosInyectados = document.getElementById("data-inyection")
+//me gusto mas la variable que dijo marce
+let listaTareas = document.getElementById("lista-tareas")
 
-let idCount = 0
+//variable inicia para aumentar el id
+let idActual = 1
 
-let datosUsuario = {
-    id: 1,
+//variable donde se guardan los datos
+datos = { 
     texto: addText,
-    fecha: addDate,
+    fecha: addDate
 }
 
-
+//necesitamos los arrays para guardar datos con push hacia el html
+let tareasP = []
+let tareasC = []
+let todasTareas = []
 
 addBtn.addEventListener('click', e => {
+    
     e.preventDefault();
-    datosInyectados.textContent = datosUsuario.id + " " + datosUsuario.texto.value + " "  + datosUsuario.fecha.value
+    //aqui llamamos a la variable directa usando los del inicio con el value para llamar la info del input
+    let nuevaTarea = idActual++ + " " + datos.texto.value + " " + datos.fecha.value
+    listaTareas.textContent = nuevaTarea
+
+    console.log(typeof nuevaTarea)
+    console.log(nuevaTarea)
 
     //limpiamos los inputs del user
-    addText.value = ' '
-    addDate.value = ' '
+    addText.value = '';
+    addDate.value = '';
+
+    //metemos a la nuevaTarea los datos guardados en las variables P y nuevas
+    todasTareas.push(nuevaTarea)
+
+    //llamamos al presionar el boton la funcion de abajo que ordena los datos hacia abajo
+    mostrar(todasTareas)
 })
+
+function mostrar(lista) {
+    var ul = document.getElementById("lista-tareas")
+
+    ul.innerHTML = '';
+    for (let i = 0; i < lista.length; i++) {
+
+        //aqui se crea un elemento para una lista li para el html
+        var li = document.createElement("li")
+
+        li.appendChild(document.createTextNode(lista[i]))
+
+        ul.appendChild(li)
+
+    }
+}
+
+// let miArray = [];
+// console.log(miArray);
+
+// let objeto1 = { texto: "comprar leche" };
+// miArray.push(objeto1);
+// console.log(miArray);
+
+// let objeto2 = { texto: "llamar a mamá" };
+// miArray.push(objeto2);
+// console.log(miArray);
+
+// console.log(miArray.length);
+// console.log(miArray[0]);
+// console.log(miArray[0].texto);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(addText);
+// console.log(addText.value);
